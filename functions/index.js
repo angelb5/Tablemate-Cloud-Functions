05867bdeec0d719124984d21994d4bbde881a45f
Angel <a20190192@pucp.edu.pe>
2022-12-08 03:15:29 -0500

    restaurantes cerca webservice

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,55 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+exports.nearbyRestaurants = functions.https
+    .onRequest(async (req, res) => {
+      const qLat = req.query.lat;
+      const qLng = req.query.lng;
+      const qRadius = req.query.radius;
+      if (!qLat || !qLng || !qRadius) {
+        // eslint-disable-next-line max-len
+        res.status(400).send({status: "FAILED", msg: "Todos los parametros son necesarios"});
+        return;
+      }
+      if (isNaN(qLat) || isNaN(qLng) || isNaN(qRadius)) {
+        // eslint-disable-next-line max-len
+        res.status(400).send({status: "FAILED", msg: "Los parámetros deben ser numéricos"});
+        return;
+      }
+      const R = 6371;
+      const latUser = parseFloat(qLat);
+      const lngUser = parseFloat(qLng);
+      const radius = parseFloat(qRadius);
+      if (radius<3 || radius>20) {
+        // eslint-disable-next-line max-len
+        res.status(400).send({status: "FAILED", msg: "El radio debe ser entre 3 a 20 km"});
+        return;
+      }
+      let nearbyRestaurants = [];
+      const restaurants = await db.collection("restaurants").get();
+      restaurants.docs.forEach((doc) => {
+        const dLat = deg2rad(doc.data().geoPoint.latitude-latUser);
+        const dLon = deg2rad(doc.data().geoPoint.longitude-lngUser);
+        // eslint-disable-next-line max-len
+        const a =Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(latUser)) * Math.cos(deg2rad(doc.data().geoPoint.latitude)) *Math.sin(dLon/2) * Math.sin(dLon/2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        const d = R * c; // Distance in km
+        if (d<=radius) {
+          const restaurant = doc.data();
+          restaurant.key = doc.id;
+          restaurant.distance = d;
+          nearbyRestaurants.push(restaurant);
+        }
+      });
+      // eslint-disable-next-line max-len
+      nearbyRestaurants = nearbyRestaurants.sort(({distance: a}, {distance: b}) => a-b);
+      res.status(200).send({status: "OK", restaurants: nearbyRestaurants});
+    });
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180);
+}
+
 exports.createRestaurantUser = functions.firestore
     .document("restaurants/{uid}")
     .onCreate((snap, context)=>{
@@ -111,7 +160,6 @@ exports.deleteRestaurant = functions.firestore
       // Get value of the newly added rating
       await deleteReviewsCollection(db, context.params.restId, 200);
     });
-
 
 async function deleteReviewsCollection(db, restaurantId, batchSize) {
   // eslint-disable-next-line max-len
